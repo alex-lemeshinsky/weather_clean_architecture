1d4b566cbba39fd2358a3680715510578d05d34c
alex-lemeshinsky <a.lemeshinsky@gmail.com>
2023-05-19 10:59:44 +0300

    git commit -m "initial commit"

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'presentation/routing/app_router.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final _appRouter = AppRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routerConfig: _appRouter.config(),
+    );
+  }
+}
